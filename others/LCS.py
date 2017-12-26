5933b271d700dc87d8f0c9895faa85c0a8b4bb26
myamamoto555 <yamamoto@soft.ics.keio.ac.jp>
2017-12-27 00:07:57 +0900

    add LCS.py

---
+++ others/LCS.py
@@ -0,0 +1,23 @@
+# coding:utf-8
+
+# 最長共通部分列
+# LCS(Longest Common Subsequence problem)
+
+
+def lcs(X, Y):
+    c = [[0 for i in range(len(Y))] for j in range(len(X))]
+    for i in range(len(X)):
+        for j in range(len(Y)):
+            if X[i] == Y[j]:
+                c[i][j] = c[i-1][j-1] + 1
+            elif c[i][j-1] >= c[i-1][j]:
+                c[i][j] = c[i][j-1]
+            else:
+                c[i][j] = c[i-1][j]
+    print c
+
+
+if __name__ == '__main__':
+    X = 'TCCAGATGG'
+    Y = 'TCACA'
+    lcs(X, Y)
